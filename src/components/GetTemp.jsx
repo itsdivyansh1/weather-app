55aa935cda3d33975e1db69a7417356b9a85bb8b
Divyansh Mishra <itsdivyansh201@gmail.com>
2024-03-24 22:02:38 +0530

    Improved responsiveness

--- src/components/GetTemp.jsx
+++ src/components/GetTemp.jsx
@@ -2,7 +2,7 @@ const GetTemp = ({ temp }) => {
   return (
     <>
       <h3>Mumbai</h3>
-      <div className="text-9xl font-bold">
+      <div className="text-9xl font-bold max-md:text-5xl">
         {temp}
         <span>°</span>
       </div>
