7e62b9953ca8e2cb0921d46de743052bcf810033
Divyansh Mishra <itsdivyansh201@gmail.com>
2024-03-25 15:24:07 +0530

    Added toggle theme feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
+  const toggleClick = () => {
+    setDarkTheme((theme) => !theme);
+    console.log(darkTheme);
+  };
+
   return (
-    <div className="bg-gradient-to-l from-[#C3E2FF] to-[#fff] w-screen h-screen">
-      <NavBar />
+    <div
+      className={
+        darkTheme
+          ? "bg-gradient-to-l from-[#C3E2FF] to-[#fff] w-screen h-screen max-md:bg-gradient-to-b"
+          : "bg-gradient-to-l from-[#6f6f6f] to-[#fff] w-screen h-screen max-md:bg-gradient-to-b "
+      }
+    >
+      <NavBar theme={darkTheme} handleButtonClick={toggleClick} />
       <Hero />
     </div>
   );
